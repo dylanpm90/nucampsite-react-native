d0e407aaf88eda216ec5f6456f9a3a1a4b630ad2
Dylan MacKay <dylan.p.mackay@gmail.com>
2021-10-22 21:06:09 -0500

    local notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -3,6 +3,7 @@ import { Text, View, ScrollView, Alert,
     Picker, Switch, Button, StyleSheet } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import * as Animatable from 'react-native-animatable';
+import * as Notifications from 'expo-notifications';
 
 class Reservation extends Component {
 
@@ -30,8 +31,34 @@ class Reservation extends Component {
         });
     }
 
+    async presentLocalNotification(date) {
+        function sendNotification() {
+            Notifications.setNotificationHandler({
+                handleNotification: async () => ({
+                    shouldShowAlert: true
+                })
+            });
+
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: 'Your Campsite Reservation Search',
+                    body: `Search for ${date} requested`
+                },
+                trigger: null
+            });
+        }
+
+        let permissions = await Notifications.getPermissionsAsync();
+        if (!permissions.granted) {
+            permissions = await Notifications.requestPermissionsAsync();
+        }
+        if (permissions.granted) {
+            sendNotification();
+        }
+    }
+
     render() {
-        return (
+        return ( 
             <ScrollView>
                 <Animatable.View animation='zoomIn' duration={2000} delay={1000}>
                     <View style={styles.formRow}>
@@ -95,8 +122,11 @@ class Reservation extends Component {
                                             style: 'cancel'
                                         },
                                         {
-                                            text: 'OK',
-                                            onPress: () => this.resetForm()
+                                            text: 'OK', 
+                                            onPress: () => {
+                                                this.presentLocalNotification(this.state.date.toLocaleDateString('en-US'));
+                                                this.resetForm();
+                                            }
                                         },
                                     ],
                                     { cancelable: false }
